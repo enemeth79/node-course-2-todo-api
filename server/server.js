6aec0936bec6df67aff2530eaf8bf9765a42f7d8
Erzsebet Nemeth <uqenemet@eait.uq.edu.au>
2017-12-15 00:58:10 +1000

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -12,12 +12,12 @@ app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
   // console.log(req.body);
-  var todo = new Todo(
-    { text: req.body.text}
-  );
+  var todo = new Todo({
+    text: req.body.text
+  });
 
   todo.save().then(
-    (doc) => { res.send(doc) },
+    (doc) => { res.send(doc); },
     (err) => { res.status(400).send(err); }
   );
 
@@ -29,3 +29,5 @@ app.post('/todos', (req, res) => {
 app.listen(3000,
   () => { console.log('Started on port 3000'); }
 );
+
+module.exports = { app };
