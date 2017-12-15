2bcd243a16daf42335632f18ae09c1874c71cebe
Erzsebet Nemeth <uqenemet@eait.uq.edu.au>
2017-12-16 01:15:04 +1000

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -32,6 +33,34 @@ app.get('/todos', (req, res) => {
       (err) => { res.status(400).send(err); }
     );
 });
+
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  //valid id using isValid
+  if (!ObjectID.isValid(id)) {
+  // 404 not found, send back empty send
+    return res.status(404).send();
+  }
+
+  //findById
+  Todo.findById(id)
+    .then(
+    //success
+    (todo) => {
+      if (!todo) {
+        // if no todo - 404 with empty body
+        return res.status(404).send();
+      }
+        // if todo - send it back
+      res.send({todo});
+    }).catch((e) => {
+      //error
+      //400 - and send empty body back
+      res.status(400).send();
+    });
+  });
 
 
 app.listen(3000,
