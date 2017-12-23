9aba3263c80b3a817d3703656b2ee20c9bbd56f9
Erzsebet Nemeth <uqenemet@eait.uq.edu.au>
2017-12-23 23:06:35 +1000

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,11 @@
 var mongoose = require('mongoose');
 
+let db = {
+  localhost: 'mongodb://localhost:27017/TodoApp',
+  mlab: 'mongodb://mymongodbuser:Ezegyjojelszo@ds131237.mlab.com:31237/node-todo-app-api'
+};
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.PORT ? db.mlab : db.localhost);
 
 module.exports = { mongoose };
