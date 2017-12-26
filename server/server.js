229f5dd7e2346f3ba3e4cc7bb81708cd90eea6e2
Erzsebet Nemeth <uqenemet@eait.uq.edu.au>
2017-12-26 23:38:06 +1000

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
@@ -90,6 +91,45 @@ app.delete('/todos/:id', (req, res) => {
         res.status(400).send();
       });
 });
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  // two properties are allowed to modify by user
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  // validate id -> not valid? return 404
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id,
+    {
+      $set: body
+    },
+    {
+      new: true
+    })
+    .then((todo) => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({todo});
+    })
+    .catch((e) => {
+      res.status(400).send();
+    })
+
+});
+
+
 
 
 app.listen(port,
