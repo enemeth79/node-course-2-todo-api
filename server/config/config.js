be38d59c4ac0186eb53b2f57bd991815843896f3
Erzsebet Nemeth <uqenemet@eait.uq.edu.au>
2018-01-18 11:33:26 +1000

    Setup separate test and development envs

---
+++ server/config/config.js
@@ -0,0 +1,11 @@
+var env = process.env.NODE_ENV || 'development';
+// console.log('env *****', env);
+if( env === 'development') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if( env === 'test') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+} else if( env === 'production' ) {
+  process.env.MONGODB_URI = 'mongodb://mymongodbuser:Ezegyjojelszo@ds131237.mlab.com:31237/node-todo-app-api';
+}
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,6 @@
 var mongoose = require('mongoose');
 
-let db = {
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  mlab: 'mongodb://mymongodbuser:Ezegyjojelszo@ds131237.mlab.com:31237/node-todo-app-api'
-};
-
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.PORT ? db.mlab : db.localhost);
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = { mongoose };
