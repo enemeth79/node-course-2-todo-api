68ca1a7f0418615a52c4c1f2d66c14096008517f
Erzsebet Nemeth <uqenemet@eait.uq.edu.au>
2018-01-24 23:52:38 +1000

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -154,6 +154,20 @@ app.get('/users/me', authenticate, (req, res) => {
 });
 
 
+app.post('/users/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+      user.generateAuthToken().then((token) => {
+        res.header('x-auth', token).send(user);
+      });
+  }).catch((e) => {
+    req.status(400).send();
+  });
+
+
+});
+
 
 app.listen(port,
   () => { console.log(`Started up at port ${port}`); }
