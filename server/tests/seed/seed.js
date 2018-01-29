1f7ff412082180cc367503984e6466e0ac421a42
Erzsebet Nemeth <uqenemet@eait.uq.edu.au>
2018-01-29 14:44:31 +1000

    Update config to use json file

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -8,10 +8,10 @@ const userTwoId = new ObjectID();
 
 const users = [
   {_id: userOneId, email: 'andrew@example.com', password: 'userOnePass',
-   tokens: [{access: 'auth', token: jwt.sign({_id: userOneId, access: 'auth'}, 'abc123').toString() }]
+   tokens: [{access: 'auth', token: jwt.sign({_id: userOneId, access: 'auth'}, process.env.JWT_SECRET).toString() }]
   },
   {_id: userTwoId, email: 'jen@example.com', password: 'userTwoPass',
-   tokens: [{access: 'auth', token: jwt.sign({_id: userTwoId, access: 'auth'}, 'abc123').toString() }]
+   tokens: [{access: 'auth', token: jwt.sign({_id: userTwoId, access: 'auth'}, process.env.JWT_SECRET).toString() }]
   }
 ];
 
